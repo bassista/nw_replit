85013c7d53aa248d592b9a96df2f25fc93b86f74
bassista <1003926+bassista@users.noreply.github.com>
2025-11-30 17:26:26 +0100

    done

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nutritionwise.app',
+  appName: 'NutritionWise',
+  webDir: 'dist/public'
+};
+
+export default config;
